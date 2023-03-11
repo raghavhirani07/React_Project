e0f883e700ce06d4537a5683d1fdc23edf29e227
RAGHAV HIRANI <90678701+raghavhirani07@users.noreply.github.com>
2023-03-11 11:14:26 +0530

    " Add navbar with link "

--- crudops/src/App.js
+++ crudops/src/App.js
@@ -1,13 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Contact from "./Component/Page/Contact";
+import Home from "./Component/Page/Home";
+import Layout from "./Component/Page/Layout";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1>Hello</h1>
-        <div>Kem cho !!</div>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<Layout />}>
+          <Route
+            index
+            element={<Home />}></Route>
+          <Route
+            path='/contact'
+            element={<Contact />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
